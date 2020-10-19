88cd50b6aa01ac610976ef39a28c65fc78c22686
millzyr <rrmills93@gmail.com>
2020-10-20 11:40:40 +1300

    Testing stuff

--- test_github_workflow.R
+++ test_github_workflow.R
@@ -1,2 +1,7 @@
 # Test script to demonstrate git workflow
 
+# Do this step if git is not setup
+use_git_config(user.name = "millzyr", user.email = "rrmills93@gmail.com")
+
+use_git(message = "This is a test commit")
+
